84d88c58b8e7658398da89a82f5ea8653d775176
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-01-16 09:57:53 +0530

    Initial commit

---
+++ lib/counter.dart
@@ -0,0 +1,9 @@
+class Counter {
+
+int _counter = 0;
+
+get count => _counter;
+
+void increment() => _counter++;
+
+}
---
+++ test/home_page_test.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+
+
+  testWidgets("Given counter is 0 When I increment counter Then counter is 1", (tester) async{});
+
+}
