0deacf20c1f9e99d2ea4e0eadaef06941aeeb1d8
akshmohan <145959795+akshmohan@users.noreply.github.com>
2025-01-16 10:04:30 +0530

    Did the first test

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -1,10 +1,22 @@
 // ignore_for_file: unused_import
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:widget_testing1/home_page.dart';
 
 void main() {
+  testWidgets("Given counter is 0 When I increment counter Then counter is 1",
+      (tester) async {
+    await tester.pumpWidget(
+    const  MaterialApp(
+        home: MyHomePage(
+          title: "Counter App",
+        ),
+      ),
+    );
 
+  final ctr =  find.text("0");
 
-  testWidgets("Given counter is 0 When I increment counter Then counter is 1", (tester) async{});
-
+  expect(ctr, findsOneWidget);
+  });
 }
